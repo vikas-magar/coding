d06706661ce1263f86d58107cd98081ff6226592
vikas-magar <vikas.b.magar@outlook.com>
2023-07-15 20:12:51 +0530

    AE | Monotonic array
    Monotonic array in O(n)

--- src/ae/monotonic_array.rs
+++ src/ae/monotonic_array.rs
@@ -1,7 +1,7 @@
 use std::vec;
 
 /* Monotonic array time: O(n) | space: O(1) */
-fn monotonic_array(v: &Vec<i32>) -> bool {
+fn monotonic_array(v: Vec<i32>) -> bool {
     let mut is_incremental: bool = false;
 
     let end = v.len();
@@ -17,16 +17,32 @@ fn monotonic_array(v: &Vec<i32>) -> bool {
     }
     return !is_incremental;
 }
+/* Monotonic Array, Time: O(n) | Space: O(1)*/
+fn is_monotonic(v: &Vec<i32>) -> bool {
+    use std::cmp::Ordering;
+    let mut d = 0;
+    for (a, b) in v.iter().zip(v[1..].iter()) {
+        d |= match a.cmp(b) {
+            Ordering::Equal => 0,
+            Ordering::Less => 1,
+            Ordering::Greater => 2,
+        };
+        if d == 3 {
+            return false;
+        }
+    }
+    return true;
+}
 
 pub fn run() {
-    let mut v = vec![-1, -5, -10, -1100, -1100, -1101, -1102, -9001];
+    let v = vec![-1, -5, -10, -1100, -1100, -1101, -1102, -9001];
     let expected = true;
-    assert_eq!(expected, monotonic_array(&mut v));
+    assert_eq!(expected, is_monotonic(&v));
 }
 
 #[test]
 fn validate() {
-    let mut v = vec![-1, -5, -10, -1100, -1100, -1101, -1102, -9001];
+    let v = vec![-1, -5, -10, -1100, -1100, -1101, -1102, -9001];
     let expected = true;
-    assert_eq!(expected, monotonic_array(&mut v));
+    assert_eq!(expected, is_monotonic(&v));
 }
