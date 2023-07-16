d615375cb62ae6e3e50b5987d7137c9e9e8c941e
Vikas Magar <vikas.b.magar@outlook.com>
2023-07-16 13:56:13 +0530

    T| update
    Testing the configuration of new network

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ use ae::move_element_end;
 use ae::smallest_diff;
 fn main() {
     println!("Practice Hard!");
+    println!("Welcome !");
     smallest_diff::sd();
     println!("Hello World!");
     move_element_end::run();
+    monotonic_array::run();
     monotonic_array::run();
 }
