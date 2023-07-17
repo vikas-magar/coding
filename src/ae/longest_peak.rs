31f6021ec7f5cdec679ad985cdbb572e5a832660
Vikas Magar <vikas.b.magar@outlook.com>
2023-07-17 20:16:46 +0530

    AE | Longest Peak
    updated test case

--- src/ae/longest_peak.rs
+++ src/ae/longest_peak.rs
@@ -1,11 +1,13 @@
 use std::{collections::HashMap, i32, usize};
 
 fn peak(a: &[i32]) -> i32 {
-    println!("{:?}", a);
     let mut max = 0;
     let mut store: HashMap<usize, &i32> = HashMap::new();
     let start = 0;
     let end = a.len() - 1;
+    if a.len() < 2 {
+        return 0;
+    }
     for (key, item) in a.iter().enumerate() {
         if key != start && key != end {
             if a[key - 1] < *item && *item > a[key + 1] {
@@ -16,21 +18,32 @@ fn peak(a: &[i32]) -> i32 {
     // down
     for (k, v) in store.iter() {
         let mut kk;
-        let mut cnt = 0;
+        let mut cnt = 1;
+        let mut left = 0;
+        let mut right = 0;
         //down
         kk = *k - 1;
-        while kk >= start && a[kk] < **v {
+        left = a[kk];
+        right = **v;
+        while kk >= start && left < right {
             cnt += 1;
             if kk == 0 {
                 break;
             }
             kk = kk - 1;
+            right = left;
+            left = a[kk];
         }
         // up
         kk = *k + 1;
-        while a[kk] > **v && kk <= end {
+        left = a[kk];
+        right = **v;
+
+        while left < right && kk <= end {
             cnt += 1;
             kk = kk + 1;
+            right = left;
+            left = a[kk];
         }
         if cnt > max {
             max = cnt;
@@ -42,6 +55,12 @@ fn peak(a: &[i32]) -> i32 {
 fn validate() {
     let a: [i32; 13] = [1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3];
     assert_eq!(6, peak(&a));
+}
+
+#[test]
+fn validate2() {
+    let a: [i32; 7] = [2, 1, 4, 7, 3, 2, 5];
+    assert_eq!(5, peak(&a));
 }
 
 pub fn run() {
