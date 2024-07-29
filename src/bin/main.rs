e7035f3ce74273dd6803cea9b2541c5c86dac994
Vikas <vikas.b.magar@outool.com>
2024-07-29 16:34:20 +0530

    added String
    starting with strings

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -28,6 +28,6 @@ fn main() {
     );
     let m = vec![0, 3, 7, 2, 5, 8, 4, 6, 0, 1];
     for ele in m {
-        println!("welcome to the india!")
+        println!("welcome to the india! {}", ele);
     }
 }
