824303efabef4fee0688dcfbc1443d947b8afb3c
Vikas <vikas.b.magar@outool.com>
2024-07-31 17:59:59 +0530

    Singly linked list
    added singly linked list

--- src/lib/linked_lists/singly_linked_list.rs
+++ src/lib/linked_lists/singly_linked_list.rs
@@ -1,46 +1,77 @@
-use std::mem;
 /// Starting with linked list
 /// Learning linked list
 /// ```assert_eq(1, 1)```
 
-pub struct List {
-    head: Link,
+pub struct List<T> {
+    head: Link<T>,
 }
 
-enum Link {
-    Empty,
-    More(Box<Node>),
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
 }
 
-struct Node {
-    elem: i32,
-    next: Link,
-}
-
-impl List {
+impl<T> List<T> {
     pub fn new() -> Self {
-        List { head: Link::Empty }
+        List { head: None }
     }
 
-    pub fn push(&mut self, elem: i32) {
+    pub fn push(&mut self, elem: T) {
         let new_node = Box::new(Node {
             elem: elem,
-            next: mem::replace(&mut self.head, Link::Empty),
+            next: self.head.take(),
         });
-        self.head = Link::More(new_node);
+        self.head = Some(new_node);
+    }
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|node| {
+            self.head = node.next;
+            node.elem
+        })
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.elem)
     }
 }
-
-fn run(inp: &Vec<i32>) {}
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::List;
 
     #[test]
     fn test_one() {
         let input = vec![1, 4, 2, 3];
-        let opt = run(&input);
-        assert_eq!(1, 1)
+        let mut ll = List::new();
+        assert_eq!(ll.pop(), None);
+        for ele in input {
+            ll.push(ele);
+        }
+
+        assert_eq!(ll.pop(), Some(3));
+    }
+
+    #[test]
+    fn peek() {
+        let mut ll = List::new();
+        assert_eq!(ll.peek(), None);
+        assert_eq!(ll.peek_mut(), None);
+        ll.push(1);
+        ll.push(2);
+        ll.push(3);
+
+        assert_eq!(ll.peek(), Some(&3));
+        assert_eq!(ll.peek_mut(), Some(&mut 3));
+
+        ll.peek_mut().map(|value| *value = 42);
+
+        assert_eq!(ll.peek(), Some(&42));
+        assert_eq!(ll.pop(), Some(42));
     }
 }
