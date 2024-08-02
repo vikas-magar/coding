9838715d8f74a1ed0073dd18dd0101f56fdac5d4
Vikas <vikas.b.magar@outool.com>
2024-08-02 06:10:07 +0530

    Singly linked list iterator
    added into iter into singly linked list

--- src/lib/linked_lists/singly_linked_list.rs
+++ src/lib/linked_lists/singly_linked_list.rs
@@ -12,8 +12,19 @@ struct Node<T> {
     elem: T,
     next: Link<T>,
 }
+pub struct IntoIter<T>(List<T>);
 
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
 impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
     pub fn new() -> Self {
         List { head: None }
     }
@@ -73,5 +84,18 @@ mod tests {
 
         assert_eq!(ll.peek(), Some(&42));
         assert_eq!(ll.pop(), Some(42));
+    }
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
     }
 }
